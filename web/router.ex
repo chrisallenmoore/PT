1b1875e0291c08a39b0368011c8fa5ed0b83badc
Chris Allen Moore <chris@chrisallenmoore.com>
2016-12-04 14:58:14 +0700

    passport normal setup

--- web/router.ex
+++ web/router.ex
@@ -1,12 +1,16 @@
 defmodule Pt.Router do
   use Pt.Web, :router
 
+  use Passport
+
   pipeline :browser do
     plug :accepts, ["html"]
     plug :fetch_session
     plug :fetch_flash
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+
+    plug :current_user
   end
 
   pipeline :api do
@@ -17,6 +21,14 @@ defmodule Pt.Router do
     pipe_through :browser # Use the default browser stack
 
     get "/", PageController, :index
+
+    get "/login", SessionController, :new
+    post "/session", SessionController, :create
+    get "/logout", SessionController, :delete
+    get "/join", RegistrationController, :new
+    post "/register", RegistrationController, :create
+    get "/passwords/new", PasswordController, :new
+    post "/passwords", PasswordController, :reset
   end
 
   # Other scopes may use custom stacks.
---
+++ web/views/registration_view.ex
@@ -0,0 +1,3 @@
+defmodule Pt.RegistrationView do
+  use Pt.Web, :view
+end
