d7d2924528defb67cb3ab8d5fcb1567f9d9061e6
Chris Allen Moore <chris@chrisallenmoore.com>
2016-12-04 13:56:35 +0700

    Created phoenix project

---
+++ config/config.exs
@@ -0,0 +1,27 @@
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Mix.Config module.
+#
+# This configuration file is loaded before any dependency and
+# is restricted to this project.
+use Mix.Config
+
+# General application configuration
+config :pt,
+  ecto_repos: [Pt.Repo]
+
+# Configures the endpoint
+config :pt, Pt.Endpoint,
+  url: [host: "localhost"],
+  secret_key_base: "m8KlcDDIlC0bgnXjDIyQhBLEZsnqRZkMuV0RYnW2/E360OJWxyPy1gN2jLs2EXMX",
+  render_errors: [view: Pt.ErrorView, accepts: ~w(html json)],
+  pubsub: [name: Pt.PubSub,
+           adapter: Phoenix.PubSub.PG2]
+
+# Configures Elixir's Logger
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{Mix.env}.exs"
---
+++ lib/pt/repo.ex
@@ -0,0 +1,3 @@
+defmodule Pt.Repo do
+  use Ecto.Repo, otp_app: :pt
+end
